6e9512ec10df0ed5e3bc0766fb07a5f8a6a2140e
Janjuks <j.locans@gmail.com>
2015-12-14 15:48:14 +0200

    Day 10 Part 1

---
+++ Day10/Day10Part1/Day10Part1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day10Part1 {
+    class Program {
+        static void Main(string[] args) {
+            string input = "1113122113";
+            for (var i = 0; i < 40; i++)
+                input = LookAndSay(input);
+            Console.WriteLine(input.Length);
+            Console.ReadKey(false);
+        }
+
+        static string LookAndSay (string input) {
+            var groups = new Regex("([0-9])\\1*").Matches(input);
+            var result = String.Empty;
+
+            foreach (var group in groups)
+                result += String.Concat(group.ToString().Length, group.ToString()[0]);
+            return result;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Day 7: Part 2 was so so similiar to Part 1 that there was no need to push this.
 Day 8: Cheated here - replaced all ASCII code representations with "AAAA", because of Regex failed attempt to parse string.
 
 Day 9: Part 1 - Woah, not a single compile until all code was written and it worked like a charm with first time :) Clean solution I'd say.
+
+Day 10: Short and easy, but didn't think it would take so much time.
